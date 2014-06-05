7c3151bfbaf7646f24fda19a1cd5bc92e9f1aab6
Manuel Abrante <racerf5@gmail.com>
2014-06-05 15:56:58 -0430

    server modules

---
+++ AplicacionesPHP/login2.php
@@ -0,0 +1,57 @@
+
+<?php
+
+header('Content-type: application/json');
+
+/* Extrae los valores enviados desde la aplicacion movil (load-json.js -->Funcion Para Login)*/
+$usuarioEnviado = $_GET['usuario'];
+$passwordEnviado = $_GET['password'];
+
+//Conexion con la base de datos
+$server = "sql310.byetcluster.com";
+$username = "mb260_13335837";
+$password = "mn4195869";
+$database = "mb260_13335837_2";
+
+$con = mysql_connect($server, $username, $password) or die ("Could not connect: " . mysql_error());
+mysql_select_db($database, $con);
+
+//Consulta a la base de datos
+$sql = "SELECT * FROM usuario where Cedula='$usuarioEnviado' and Password='$passwordEnviado'";
+//$sql = "SELECT * FROM usuario where Cedula='7100894' AND Password='7100894'";
+$result = mysql_query($sql) or die ("Query error: " . mysql_error());
+
+//Variable que guardará cuantos datos arroja la base de datos
+$cantidad=0;
+
+while($row = mysql_fetch_row($result)) {
+	$cantidad++;
+	$tipo = $row[1];
+	$c = $row[0];
+
+}
+ 
+  
+$resultados["validacion"] = $cantidad;
+$resultados["T"] = $tipo;
+
+if($cantidad>0 && $tipo== 1){
+	setcookie("usuario",$c,time()+36000);
+	$resultados["mensaje"] = "Bienvenido $usuarioEnviado";
+}
+else
+if($cantidad>0 && $tipo== 2){
+	//$resultados["mensaje"] = "Administrador $usuarioEnviado";
+	setcookie("usuario",$c,time()+36000);
+	$resultados["mensaje"] = "Profesor $usuarioEnviado";
+}
+else
+	$resultados["mensaje"] = "Usuario o Password Incorrectos";
+
+/*Convierte los resultados a formato json*/
+$resultadosJson = json_encode($resultados);
+
+/*Muestra el resultado en un formato que no da problemas de seguridad en browsers */
+echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
+
+?>
---
+++ AplicacionesPHP/verHorario.php
@@ -0,0 +1,33 @@
+<?php
+
+header('Content-type: application/json');
+
+/* Extrae los valores enviados desde la aplicacion movil (load-json.js -->Funcion agregarHora)*/
+//$cedulaEnviada = $_GET['cedulaJ'];
+$cedulaEnviada = $_COOKIE['usuario'];
+//Conexion con la base de datos
+$server = "sql310.byetcluster.com";
+$username = "mb260_13335837";
+$password = "mn4195869";
+$database = "mb260_13335837_2";
+
+$con = mysql_connect($server, $username, $password) or die ("Could not connect: " . mysql_error());
+mysql_select_db($database, $con);
+
+//Consulta a la base de datos
+$sql = "SELECT H.Hora_Inicio, H.Hora_Fin, F.Dia, N.Nombre_Materia, U.Nombre FROM estudiantehorariocarreramateria B INNER JOIN horariocarreramateria C ON B.Id_HorarioCarreraMateria_E=C.Id_horarioCarreraMateria INNER JOIN carreramateria M ON C.Id_carrera_Materia_H = M.Id_Carrera_Materia INNER JOIN horario R ON C.Id_Horario_M = R.Id_Horario INNER JOIN hora H ON R.Id_Hora_Horario = H.Id_Hora INNER JOIN fecha F ON R.Id_Fecha_Horario = F.Id_Fecha INNER JOIN materias N ON M.Id_Materia=N.Id_Materia INNER JOIN usuario U ON B.Cedula=U.Cedula WHERE B.Cedula='$cedulaEnviada' ORDER BY F.Id_Fecha ASC";
+
+$result = mysql_query($sql) or die ("Query error: " . mysql_error());
+$records = array();
+
+while($row = mysql_fetch_assoc($result)) {
+	$records[] = $row;
+
+}
+
+/*Convierte los resultados a formato json*/
+$resultadosJson = json_encode($records);
+/*Muestra el resultado en un formato que no da problemas de seguridad en browsers */
+echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
+
+?>
